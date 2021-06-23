e2c48eb9b29dea2e214e06817414dc72012e157e
marcusRB <mrussorb@gmail.com>
2021-06-23 10:49:03 +0000

    update DEV

--- build/build_dockerfile.sh
+++ build/build_dockerfile.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 #
 # -- Build Apache Spark Standalone Cluster Docker Images
-
+# Versión 1.2.0 - 2021-06-19
+# add requirements.txt
+# add MLflow
+# remove Hive image
+# fixed docker build tags
 # ----------------------------------------------------------------------------------------------------------------------
 # -- Variables ---------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
+BUILD_VERSION="v1.2.0"
 BUILD_DATE="$(date -u +'%Y-%m-%d')"
 
 SHOULD_BUILD_BASE="$(grep -m 1 build_base build.yml | grep -o -P '(?<=").*(?=")')"
 SHOULD_BUILD_SPARK="$(grep -m 1 build_spark build.yml | grep -o -P '(?<=").*(?=")')"
-# SHOULD_BUILD_SPARK_HIVE="$(grep -m 1 build_spark_hive build.yml | grep -o -P '(?<=").*(?=")')"
 SHOULD_BUILD_JUPYTERLAB="$(grep -m 1 build_jupyter build.yml | grep -o -P '(?<=").*(?=")')"
 
 SPARK_VERSION="$(grep -m 1 spark build.yml | grep -o -P '(?<=").*(?=")')"
@@ -41,34 +45,29 @@ fi
 
 function cleanContainers() {
 
-    container="$(docker ps -a | grep 'jupyterlab' | awk '{print $1}')"
+    container="$(docker ps -a | grep 'mrussorb/cluster-jupyterlab:${JUPYTERLAB_VERSION}-spark${SPARK_VERSION}-${BUILD_VERSION}' | awk '{print $1}')"
     docker stop "${container}"
     docker rm "${container}"
 
-    container="$(docker ps -a | grep 'spark-worker' -m 1 | awk '{print $1}')"
+    container="$(docker ps -a | grep 'mrussorb/cluster-spark-worker:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION}' -m 1 | awk '{print $1}')"
     while [ -n "${container}" ];
     do
       docker stop "${container}"
       docker rm "${container}"
-      container="$(docker ps -a | grep 'spark-worker' -m 1 | awk '{print $1}')"
+      container="$(docker ps -a | grep 'mrussorb/cluster-spark-worker:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION}' -m 1 | awk '{print $1}')"
     done
 
-    container="$(docker ps -a | grep 'spark-master' | awk '{print $1}')"
+    container="$(docker ps -a | grep 'mrussorb/cluster-spark-master:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION}' | awk '{print $1}')"
     docker stop "${container}"
     docker rm "${container}"
 
-    container="$(docker ps -a | grep 'spark-base' | awk '{print $1}')"
+    container="$(docker ps -a | grep 'mrussorb/cluster-spark-base:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION}' | awk '{print $1}')"
     docker stop "${container}"
     docker rm "${container}"
 
-    # container="$(docker ps -a | grep 'spark-base-hive' | awk '{print $1}')"
-    # docker stop "${container}"
-    # docker rm "${container}"
-
-
-    # container="$(docker ps -a | grep 'base' | awk '{print $1}')"
-    # docker stop "${container}"
-    # docker rm "${container}"
+    container="$(docker ps -a | grep 'base' | awk '{print $1}')"
+    docker stop "${container}"
+    docker rm "${container}"
 
 }
 
@@ -76,27 +75,20 @@ function cleanImages() {
 
     if [[ "${SHOULD_BUILD_JUPYTERLAB}" == "true" ]]
     then
-      docker rmi -f "$(docker images | grep -m 1 'jupyterlab:${JUPYTERLAB_VERSION}-spark-${SPARK_VERSION}' | awk '{print $3}')"
+      docker rmi -f "$(docker images | grep -m 1 'mrussorb/cluster-jupyterlab:${JUPYTERLAB_VERSION}-spark${SPARK_VERSION}-${BUILD_VERSION}' | awk '{print $3}')"
     fi
 
     if [[ "${SHOULD_BUILD_SPARK}" == "true" ]]
     then
-      docker rmi -f "$(docker images | grep -m 1 'spark-worker' | awk '{print $3}')"
-      docker rmi -f "$(docker images | grep -m 1 'spark-master' | awk '{print $3}')"
-      docker rmi -f "$(docker images | grep -m 1 'spark-base' | awk '{print $3}')"
+      docker rmi -f "$(docker images | grep -m 1 'mrussorb/cluster-spark-worker:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION}' | awk '{print $3}')"
+      docker rmi -f "$(docker images | grep -m 1 'mrussorb/cluster-spark-master:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION}' | awk '{print $3}')"
+      docker rmi -f "$(docker images | grep -m 1 'mrussorb/cluster-spark-base:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION}' | awk '{print $3}')"
     fi
 
-    # if [[ "${SHOULD_BUILD_SPARK_HIVE}" == "true" ]]
-    # then
-    #   # docker rmi -f "$(docker images | grep -m 1 'spark-worker' | awk '{print $3}')"
-    #   # docker rmi -f "$(docker images | grep -m 1 'spark-master' | awk '{print $3}')"
-    #   docker rmi -f "$(docker images | grep -m 1 'spark-base-hive' | awk '{print $3}')"
-    # fi
-
-    # if [[ "${SHOULD_BUILD_BASE}" == "true" ]]
-    # then
-    #   docker rmi -f "$(docker images | grep -m 1 'base' | awk '{print $3}')"
-    # fi
+    if [[ "${SHOULD_BUILD_BASE}" == "true" ]]
+    then
+      docker rmi -f "$(docker images | grep -m 1 'mrussorb/cluster-base:debian_buster-${BUILD_VERSION}' | awk '{print $3}')"
+    fi
 
 }
 
@@ -106,14 +98,15 @@ function cleanVolume() {
 
 function buildImages() {
 
-  # if [[ "${SHOULD_BUILD_BASE}" == "true" ]]
-  # then
-  #   docker build \
-  #     --build-arg build_date="${BUILD_DATE}" \
-  #     --build-arg scala_version="${SCALA_VERSION}" \
-  #     -f docker/base/Dockerfile \
-  #     -t base:latest .
-  # fi
+  if [[ "${SHOULD_BUILD_BASE}" == "true" ]]
+  then
+    docker build \
+      --build-arg build_date="${BUILD_DATE}" \
+      --build-arg scala_version="${SCALA_VERSION}" \
+      --build-arg build_version="${BUILD_VERSION}" \
+      -f docker/base/Dockerfile \
+      -t mrussorb/cluster-base:debian_buster-${BUILD_VERSION} .
+  fi
 
   if [[ "${SHOULD_BUILD_SPARK}" == "true" ]]
   then
@@ -123,22 +116,25 @@ function buildImages() {
       --build-arg spark_version="${SPARK_VERSION}" \
       --build-arg hadoop_version="${HADOOP_VERSION}" \
       --build-arg postgres_version="${POSTGRESQL_VERSION}" \
+      --build-arg build_version="${BUILD_VERSION}" \
       -f docker/spark-base/Dockerfile \
-      -t cluster-spark-base:${SPARK_VERSION}-hadoop${HADOOP_VERSION} .
-  
-    docker build \
-      --build-arg build_date="${BUILD_DATE}" \
-      --build-arg spark_version="${SPARK_VERSION}" \
-      --build-arg hadoop_version="${HADOOP_VERSION}" \
-      -f docker/spark-master/Dockerfile \
-      -t cluster-spark-master:${SPARK_VERSION}-hadoop${HADOOP_VERSION} .
+      -t mrussorb/cluster-spark-base:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION} .
 
     docker build \
       --build-arg build_date="${BUILD_DATE}" \
       --build-arg spark_version="${SPARK_VERSION}" \
-       --build-arg hadoop_version="${HADOOP_VERSION}" \
+      --build-arg hadoop_version="${HADOOP_VERSION}" \
+      --build-arg build_version="${BUILD_VERSION}" \
+      -f docker/spark-master/Dockerfile \
+      -t mrussorb/cluster-spark-master:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION} .
+
+    docker build \
+      --build-arg build_date="${BUILD_DATE}" \
+      --build-arg spark_version="${SPARK_VERSION}" \
+      --build-arg hadoop_version="${HADOOP_VERSION}" \
+      --build-arg build_version="${BUILD_VERSION}" \
       -f docker/spark-worker/Dockerfile \
-      -t cluster-spark-worker:${SPARK_VERSION}-hadoop${HADOOP_VERSION} .
+      -t mrussorb/cluster-spark-worker:${SPARK_VERSION}-hadoop${HADOOP_VERSION}-${BUILD_VERSION} .
 
   fi
 
@@ -150,8 +146,9 @@ function buildImages() {
       --build-arg spark_version="${SPARK_VERSION}" \
       --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
       --build-arg scala_kernel_version="${SCALA_KERNEL_VERSION}" \
+      --build-arg build_version="${BUILD_VERSION}" \
       -f docker/jupyterlab/Dockerfile \
-      -t jupyterlab:${JUPYTERLAB_VERSION}-spark-${SPARK_VERSION} .
+      -t mrussorb/cluster-jupyterlab:${JUPYTERLAB_VERSION}-spark${SPARK_VERSION}-${BUILD_VERSION} .
   fi
 
 }
